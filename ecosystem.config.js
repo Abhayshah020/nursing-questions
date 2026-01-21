c03e99facf823e1c6e0fa2753dc7ddfb8c31cf56
Abhay Shah <seraski2021@gmail.com>
2026-01-21 18:34:49 +0545

    completed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  apps: [
-    {
-      name: "nursing-frontend",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 4051", // Port you want to run
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+    apps: [
+        {
+            name: "nursing-frontend",
+            script: "node_modules/next/dist/bin/next",
+            cwd: "/var/www/nursing-questions",
+            args: "start -p 4051", // Port you want to run
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    ],
 };
